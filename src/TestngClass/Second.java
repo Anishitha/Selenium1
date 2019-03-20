1b216eae6b770810ce5744d5bb3c213bcd771985
Anishitha <kunduranishitha@gmail.com>
2019-03-20 21:56:56 +0530

    testng

---
+++ src/TestngClass/Second.java
@@ -0,0 +1,18 @@
+package TestngClass;
+
+import org.testng.annotations.Test;
+
+public class Second {
+
+	@Test
+	public void testcase01() {
+		System.out.println("I am in testcase1");
+	}
+	
+	@Test
+	public void testcase02() {
+		System.out.println("I am in testcase2");
+	}
+	
+	
+}
---
+++ src/TestngClass/Third.java
@@ -0,0 +1,16 @@
+package TestngClass;
+
+import org.testng.annotations.Test;
+
+public class Third {
+
+	@Test
+	public void testcase03() {
+		System.out.println("I am in testcase3");
+	}
+	
+	@Test
+	public void testcase04() {
+		System.out.println("I am in testcase4");
+	}
+}
