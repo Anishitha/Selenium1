a7abd33977a0299411209f94add8453a81e04b58
Anishitha <kunduranishitha@gmail.com>
2019-03-27 08:00:08 +0530

    testng

--- src/TestngClass/TestngFirst.java
+++ src/TestngClass/TestngFirst.java
@@ -5,30 +5,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestngFirst {
 	
-	WebDriver driver = new ChromeDriver();
+	WebDriver driver = null;
 	
 	
 	@BeforeTest
 	public void beforeTheTest() {
-	System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
-	driver.get("https://www.amazon.in/");
+		System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+
+		driver.get("https://www.amazon.in/");
 	}
 	
 	@Test
 	public void testcase01(){
 		WebElement searchElement = driver.findElement(By.id("twotabsearchtextbox"));	
-		searchElement.sendKeys("iPhone");
-		driver.findElement(By.className("nav-input")).click();;
+		searchElement.sendKeys("iphone");
+		driver.findElement(By.className("nav-input")).click();
+		
 	}
 	
 	@Test
 	public void testcase02(){
-		driver.findElement(By.linkText("Click Here")).click();
+		
+		driver.findElement(By.linkText("Amazon Pay")).click();
 	}
 	
 	@Test
